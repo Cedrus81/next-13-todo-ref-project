1ba014032e64fefffeb9986091a795a85d9cb5c7
Cedrus81 <erez.eitan.97@gmail.com>
2023-05-25 11:48:35 +0300

    final commit, added update todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,11 @@ async function getTodos(): Promise<Todo[]> {
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id:string, complete: boolean) {
+  'use server'
+  await prisma.todo.update({where: {id}, data: {complete}})
+}
+
 export default async function Home() {
   const todos = await getTodos()
   return (
@@ -18,7 +23,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
